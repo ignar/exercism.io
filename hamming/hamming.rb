4c82588d9036e29d59a666effec949e70bea6d1f
Artem Melnykov <melnikov.artem.v@gmail.com>
2019-03-04 07:22:42 +0100

    Hamming
    refactor a bit

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,11 +1,9 @@
 module Hamming
   def self.compute(str1, str2)
     raise ArgumentError if str1.length != str2.length
-    arr1 = str1.split('')
-    arr2 = str2.split('')
     mapped = []
-    arr1.each_with_index do |chr, index|
-      mapped << (arr2[index] != chr)
+    0.upto(str1.length) do |index|
+      mapped << (str1[index] != str2[index])
     end
     mapped.reduce(0) { |memo, el| memo += 1 if el; memo }
   end
